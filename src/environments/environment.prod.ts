271b383a14302bc75199f38fccf4c9b4c472e7c2
Saima Chaity <saima.s.chaity@gmail.com>
2019-02-19 09:34:35 -0800

    Added media queries in css style

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBzhq3XR1nD8ssJ7g09xfI-vFih4Cn6wYo',
+    authDomain: 'chatapp-6b684.firebaseapp.com',
+    databaseURL: 'https://chatapp-6b684.firebaseio.com',
+    projectId: 'chatapp-6b684',
+    storageBucket: 'chatapp-6b684.appspot.com',
+    messagingSenderId: '598928909545'
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBzhq3XR1nD8ssJ7g09xfI-vFih4Cn6wYo',
+    authDomain: 'chatapp-6b684.firebaseapp.com',
+    databaseURL: 'https://chatapp-6b684.firebaseio.com',
+    projectId: 'chatapp-6b684',
+    storageBucket: 'chatapp-6b684.appspot.com',
+    messagingSenderId: '598928909545'
+  }
+};
+
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
